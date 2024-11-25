a5acbbb4d26b22f7a19e8855d96c4070f02e8c6c
nicoledeklerk <nicolenataliedeklerk@gmail.com>
2024-11-25 12:48:57 +0200

    Implement displayMenuItems(menu) function
    Created a function that creates HTML elements to display menu items grouped by category

--- index.js
+++ index.js
@@ -16,30 +16,30 @@ const prices = {
 
 // Function to display menu items by category
 function displayMenuItems(menu) {
-    // Get the menu container element from the HTML
+    const menuContainer = document.getElementById("menu");
 
-    // Loop through each category and its items in the menu object
+    // Clear existing content
+    menuContainer.innerHTML = '';
 
-        // Create an element to represent the category
+    for (const category in menu) {
+        
+    // Create and append the category element
+    const categoryElement = document.createElement("h3");
+     categoryElement.textContent = category; 
+     menuContainer.appendChild(categoryElement);
 
-        // Set the text content of the category element to the category name
+     // Create and append the list of items
+     const listElement = document.createElement("ul"); 
+     menu[category].forEach((item) => {
 
-        // Append the category element to the menu container
+    // Create and append the list item element
+    const listItemElement = document.createElement("li"); 
+    listItemElement.textContent = item; 
+    listItemElement.addEventListener("click", () => addToOrder(item)); listElement.appendChild(listItemElement);
+});
 
-        // Create an element to represent a list of items
-
-        // Append a list of items element to the menu container
-
-        // Loop through the items in the category and create list items
-
-            // Create a list item element
-
-            // Set the text content of the list item element to the item name
-
-            // Attach a click event listener to the list item to add it to the order
-
-            // Append the list item to the list of items
-
+menuContainer.appendChild(listElement); 
+}
             
 }
 
